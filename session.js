8dc98ff0d09195edbd77f1ba3215875d1a1d28e3
dhanuwanew <161698816+dhanuwanew@users.noreply.github.com>
2025-05-11 19:16:12 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,12 +40,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
-    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~bMVVjQyb#9hWNopxwN9XCl21tFBnmLJHvx8TxhTKqgGg6S5r-TJU",
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "d2P2La3MAtZNnYefMBi0gTTBbxB9u61ypWCy",
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "dhanuwanew",
+    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94761047254", "94701669543"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "session",
-    FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
+    FOOTER: process.env.FOOTER || "> 𝚀𝚄𝙴𝙴𝙽 𝚃𝙷𝙰𝚁𝚄𝚄👸 𓄂𝐎ᴡͥɳͣᴇͫᴙ《 𝐃𝚮𝚫𝚴𝐔𝐖𝚫 》" 
   };
   
